754c247cc8fff617ba698b30a81e42a8c24dfdc2
Maja170925 <mmaja172005@gmail.com>
2025-06-05 21:44:42 +0200

    chore: the code was restructured according to the mvc template

--- main.py
+++ main.py
@@ -1,15 +1,10 @@
+from utils.model import toll_both
+from utils.controller import get_both_info
 
 
-toll_both : list = [
 
-    {'name' : 'SPO Olszowa', 'location' : 'Olszowa'},
-    {'name' : 'SPO Nogowczyce', 'location' : 'Nogowczyce'},
-    {'name' : 'SPO Łany', 'location' : 'Łany'},
-    {'name' : 'SPO Ostropa', 'location' : 'Ostropa'},
-]
+def main():
+    get_both_info(toll_both)
 
-def get_both_info(both_data : list) -> None:
-    for both in both_data:
-        print(f'{both['name']} : {both["location"]}')
-
-get_both_info(toll_both)
+if __name__ == '__main__':
+    main()
