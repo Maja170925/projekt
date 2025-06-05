07bbe9b7e7a1dcc9d321fbce6e4ef38f3b39fda4
Maja170925 <mmaja172005@gmail.com>
2025-06-05 23:13:08 +0200

    wip: add update booth functionality

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from utils.model import toll_both
-from utils.controller import get_both_info, add_both_info, remove_both_info
+from utils.model import toll_booth
+from utils.controller import get_booth_info, add_booth_info, remove_booth_info, update_booth_info
 
 
 def main():
@@ -8,14 +8,16 @@ def main():
     print('1 - wyświetl dane punktów poboru opłat')
     print('2 - dodaj punkt poboru opłat')
     print('3 - usuń punkt poboru opłat')
+    print('4 - zaktualizuj punkt poboru opłat')
     print('=================')
-    get_both_info(toll_both)
+    get_booth_info(toll_booth)
     while True:
         choice: str = input('wybierz opcję MENU: ')
         if choice == '0': break
-        if choice == '1': get_both_info(toll_both)
-        if choice == '2': add_both_info(toll_both)
-        if choice == '3': remove_both_info(toll_both)
+        if choice == '1': get_booth_info(toll_booth)
+        if choice == '2': add_booth_info(toll_booth)
+        if choice == '3': remove_booth_info(toll_booth)
+        if choice == '4': update_booth_info(toll_booth)
 
 if __name__ == '__main__':
     main()
