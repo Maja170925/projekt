be6f64311a23c92315c49c6f542294e6fd3189e6
Maja170925 <mmaja172005@gmail.com>
2025-06-05 22:34:29 +0200

    wip: add new both functionality

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
 from utils.model import toll_both
-from utils.controller import get_both_info
+from utils.controller import get_both_info, add_both_info
 
 
 def main():
     print('=========MENU=========')
     print('0 - zakończ program')
     print('1 - wyświetl dane punktów poboru opłat')
+    print('2 - dodaj punkt poboru opłat')
     print('=================')
     get_both_info(toll_both)
     while True:
         choice: str = input('wybierz opcję MENU: ')
         if choice == '0': break
         if choice == '1': get_both_info(toll_both)
+        if choice == '2': add_both_info(toll_both)
 
-main()
+if __name__ == '__main__':
+    main()
 
--- utils/controller.py
+++ utils/controller.py
@@ -2,3 +2,8 @@ def get_both_info(both_data : list) -> None:
     for both in both_data:
         print(f'{both['name']} : {both["location"]}')
 
+
+def add_both_info(both_data : list) -> None:
+    new_name: str = input('Podaj nazwę punktu poboru opłat')
+    new_location: str = input('Podaj lokalizację punktu poboru opłat')
+    both_data.append({'name': new_name, 'location': new_location})
