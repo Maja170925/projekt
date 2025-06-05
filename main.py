edda701fffec8021368445afbbebfda9cc9f127b
Maja170925 <mmaja172005@gmail.com>
2025-06-05 22:00:12 +0200

    wip: add MENU

--- main.py
+++ main.py
@@ -2,9 +2,16 @@ from utils.model import toll_both
 from utils.controller import get_both_info
 
 
-
 def main():
+    print('=========MENU=========')
+    print('0 - zakończ program')
+    print('1 - wyświetl dane punktów poboru opłat')
+    print('=================')
     get_both_info(toll_both)
+    while True:
+        choice: str = input('wybierz opcję MENU: ')
+        if choice == '0': break
+        if choice == '1': get_both_info(toll_both)
 
-if __name__ == '__main__':
-    main()
+main()
+
