637c56956522fb1e797ed8a2a2f0229458d2731f
Maja170925 <mmaja172005@gmail.com>
2025-06-19 22:43:08 +0200

    wip: test3

--- utils/model.py
+++ utils/model.py
@@ -5,5 +5,5 @@ toll_booth : list = [
     {'name' : 'SPO Olszowa', 'location' : 'Olszowa'},
     {'name' : 'SPO Nogowczyce', 'location' : 'Nogowczyce'},
     {'name' : 'SPO Łany', 'location' : 'Łany'},
-    {'name' : 'SPO Ostropa', 'location' : 'Tadki'},
+    {'name' : 'SPO Ostropa', 'location' : 'k'},
 ]
